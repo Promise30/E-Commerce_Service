0059700564480d3f25809c688dde10adf8957a69
Promise30 <promiseadeagbo@gmail.com>
2025-01-06 15:42:55 +0100

    Configure initial required services in the pipeline

--- ECommerceService.API/Program.cs
+++ ECommerceService.API/Program.cs
@@ -1,11 +1,29 @@
+using ECommerceService.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configure Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add configuration based on environment
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+// Configure Automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Configure Database
+builder.Services.AddDbContext<ECommerceDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
@@ -15,7 +33,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+// Apply migrations to database
+using(var service =  app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    var context = service.ServiceProvider.GetRequiredService<ECommerceDbContext>();
+    context.Database.Migrate();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
