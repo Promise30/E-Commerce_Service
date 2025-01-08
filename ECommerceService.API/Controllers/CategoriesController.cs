6864db6d6e544d6c887f303f810f2926fa26cd54
Promise30 <promiseadeagbo@gmail.com>
2025-01-08 10:03:45 +0100

    Controllers for Category and Product entities with CRUD operations

---
+++ ECommerceService.API/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using ECommerceService.API.Application.Interfaces;
+using ECommerceService.API.Data.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategoriesAsync()
+        {
+            var response = await _categoryService.GetAllCategoriesAsync();
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpGet("{categoryId}")]
+        public async Task<IActionResult> GetCategory(int categoryId)
+        {
+            var response = await _categoryService.GetCategory(categoryId);
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryDto createCategory)
+        {
+            if(!ModelState.IsValid)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Invalid data provided");
+            }
+            var response = await _categoryService.CreateCategory(createCategory);
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpPut("{categoryId}")]
+        public async Task<IActionResult> UpdateCategory(int categoryId, [FromBody] UpdateCategoryDto updateCategory)
+        {
+            if(!ModelState.IsValid)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Invalid data provided");
+            }
+            var response = await _categoryService.UpdateCategory(categoryId, updateCategory);
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpDelete("{categoryId}")]
+        public async Task<IActionResult> DeleteCategory(int categoryId)
+        {
+            var response = await _categoryService.DeleteCategory(categoryId);
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
---
+++ ECommerceService.API/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using ECommerceService.API.Application.Interfaces;
+using ECommerceService.API.Data.Dtos;
+using ECommerceService.API.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductsAsync()
+        {
+            var response = await _productService.GetAllProductsAsync();
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> GetProduct(Guid productId)
+        {
+            var response = await _productService.GetProductAsync(productId);
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto createProduct)
+        {
+            var response = await _productService.CreateProduct(createProduct);
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpPut("{productId}")]
+        public async Task<IActionResult> UpdateProduct(Guid productId, [FromBody] UpdateProductDto updateProduct)
+        {
+            var response = await _productService.UpdateProduct(productId, updateProduct);
+            return StatusCode((int)response.StatusCode, response);
+        }
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> DeleteProduct(Guid productId)
+        {
+            var response = await _productService.DeleteProduct(productId);
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
