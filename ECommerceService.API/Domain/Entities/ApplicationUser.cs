ffc4887c84288bda7b6611fe3713698fab379375
Promise30 <promiseadeagbo@gmail.com>
2025-01-12 21:58:48 +0100

    Extended IdentityUser for project

---
+++ ECommerceService.API/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ECommerceService.API.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? PhoneCountryCode { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset LastModifiedDate { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryDate { get; set; }
+
+    }
+}
