7f9d8047685ca3479a0202a0bff4726a6ec9c965
Promise30 <promiseadeagbo@gmail.com>
2025-04-01 03:53:21 +0100

    Added health check related files

---
+++ ECommerceService.API/HealthChecks/CustomHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ECommerceService.API.HealthChecks
+{
+    public class CustomHealthCheck : IHealthCheck
+    {
+        private readonly string _connectionString;
+
+        public CustomHealthCheck(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            _connectionString = connectionString?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync(cancellationToken);
+                    return HealthCheckResult.Healthy("Database is reachable");
+                }
+                catch(SqlException ex)
+                {
+                    return HealthCheckResult.Unhealthy("Unable to connect to database", ex);
+                }
+            }
+        }
+    }
+}
---
+++ ECommerceService.API/HealthChecks/HealthCheck.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ECommerceService.API.HealthChecks
+{
+    public static  class HealthCheck
+    {
+        public static void ConfigureHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("DefaultConnection")!,
+                        name: "SQL Server",
+                        healthQuery: "SELECT 1;",
+                        failureStatus:HealthStatus.Unhealthy,
+                        tags: new[] {"Feedback", "Database"});
+            services.AddHealthChecksUI(opt =>
+            {
+                opt.SetEvaluationTimeInSeconds(15);
+                opt.MaximumHistoryEntriesPerEndpoint(60);
+                opt.AddHealthCheckEndpoint("ECommerceService API", "/api/health");
+            }).AddInMemoryStorage();
+
+        }
+    }
+}
