30e8f7d52ab5a0dd95f21fd4c8c2d2a58f894bdf
Promise30 <promiseadeagbo@gmail.com>
2025-04-01 03:59:32 +0100

    Modified orderitem dto file

--- ECommerceService.API/Data/Dtos/Order/OrderItemDto.cs
+++ ECommerceService.API/Data/Dtos/Order/OrderItemDto.cs
@@ -5,9 +5,7 @@
         public int Id { get; set; }
         public int OrderId { get; set; }
         public int ProductId { get; set; }
-        //public string ProductName { get; set; }
         public decimal UnitPrice { get; set; }
-        //public decimal DiscountApplied { get; set; }
         public int Quantity { get; set; }
         public decimal SubTotal { get; set; }
         public DateTimeOffset DateCreated { get; set; }
